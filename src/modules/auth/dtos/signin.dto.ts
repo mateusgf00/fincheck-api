fcff2ba14c2e72b9d4423263d438ab5074133a73
Mateus <mateusgf00@gmail.com>
2024-05-20 17:12:44 -0300

    create jwt token and authentication

--- src/modules/auth/dtos/signin.dto.ts
+++ src/modules/auth/dtos/signin.dto.ts
@@ -1,9 +1,6 @@
 import { IsEmail, IsNotEmpty, IsString, MinLength } from "class-validator";
 
-export class AuthSignUpDto {
-  @IsString()
-  @IsNotEmpty()
-  name: string;
+export class SigninDto {
 
   @IsString()
   @IsNotEmpty()
--- src/modules/auth/dtos/signup.dto.ts
+++ src/modules/auth/dtos/signup.dto.ts
@@ -1,6 +1,6 @@
 import { IsEmail, IsNotEmpty, IsString, MinLength } from "class-validator";
 
-export class AuthSignUpDto {
+export class SignupDto {
   @IsString()
   @IsNotEmpty()
   name: string;
